d436649b98256780005ff9f6d7dad719759c79f3
400kingz <flurpro@gmail.com>
2023-05-23 02:14:37 -0700

    backend overhaul

--- server.js
+++ server.js
@@ -1,62 +1,53 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-
-require("dotenv").config();
-
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  organization: "org-POwYe8cvtR3ywvUJenzumikx",
-  apiKey: process.env.OPENAI_SECRET_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
+const express = require('express');
 const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
+const axios = require('axios');
+const { promisify } = require('util');
+const cache = require('memory-cache');
+const bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Set up a rate limiter
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10 // limit each IP to 10 requests per windowMs
 });
 
-app.post("/chat", async (req, res) => {
-  const { prompt } = req.body;
-  console.log("Received request:", req.body);
+// Apply rate limiter to all requests
+app.use(limiter);
 
-  // Create an array of messages with the user's prompt
-  const messages = [
-    {
-      role: "system",
-      content:
-        'You are now ProgramBot, an AI tool designed to create engaging and unique programs for middle school young adults aged 11-14, for use with organizations like the Boys and Girls Club and schools. You must address these young adults as "Members" only. Each activity you create must focus on one of the following categories: Leadership and Service, Educational and STEM, College and Career, Health and Wellness, The Arts, or Sports and Recreation. For each activity, provide ALL of the following details and in this order: Session Name , Lesson Objectives , Lesson Materials , Preparation , Community Builder , Main Activity , Reflection. Please create an activity based on the given prompt.',
-    },
-    {
-      role: "user",
-      content: prompt,
-    },
-  ];
+// Set up body parser for parsing JSON
+app.use(bodyParser.json());
 
+app.post('/chat', async (req, res) => {
+  const prompt = req.body.prompt;
+  
+  // Check the cache first
+  const cachedResponse = cache.get(prompt);
+  if (cachedResponse) {
+    return res.send(cachedResponse);
+  }
+  
   try {
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: messages,
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      max_tokens: 1500,
       temperature: 0.5,
+      prompt: prompt
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_KEY}`,
+        'Content-Type': 'application/json'
+      }
     });
 
-    return res.send(completion.data.choices[0].message.content);
+    // Cache the response
+    cache.put(prompt, response.data, 60 * 1000);  // Cache for 1 minute
+    
+    res.send(response.data);
   } catch (error) {
-    console.error("Error:", error);
-    console.error("Error response:", error.response);
-    return res.status(500).send(`Error: ${error.message}`);
+    console.error(`Failed to fetch chat response: ${error}`);
+    res.status(500).send('Failed to fetch chat response');
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
